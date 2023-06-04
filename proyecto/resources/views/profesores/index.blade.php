e567e6af764399514e72c2014a18d8473a04ba56
attz10 <94582755+attz10@users.noreply.github.com>
2023-06-04 15:33:14 -0400

    agregue extends a la vista de profesores

--- proyecto/resources/views/profesores/index.blade.php
+++ proyecto/resources/views/profesores/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link rel="stylesheet" href="{{asset('css/custom-boostrap.min.css')}}">
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> --}}
-</head>
-<body>
-    
+@extends('layouts.master')
+
+@section('contenido-principal')
     <div class="row">
         <div class="container-fluid">
             <div class="col bg-light">
@@ -49,5 +41,4 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
